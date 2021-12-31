376551d33d3f649d45449887190ad38dae8d3425
ding.feng <ding.feng@immomo.com>
2021-12-31 11:13:31 +0800

    新增：增加非法url提示

--- app/src/main/java/com/example/helloworld/lua/LuaActivity.kt
+++ app/src/main/java/com/example/helloworld/lua/LuaActivity.kt
@@ -3,8 +3,11 @@ package com.example.helloworld.lua
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.helloworld.R
 import com.example.helloworld.lua.utils.LuaUtils
+import com.immomo.luanative.hotreload.HotReloadServer
+import com.immomo.mls.HotReloadHelper
 import com.immomo.mls.InitData
 import com.immomo.mls.MLSInstance
 import java.io.File
@@ -23,6 +26,9 @@ class LuaActivity : AppCompatActivity() {
         instance.setContainer(findViewById(R.id.root_view))
         val initData = InitData(entryPath)
         instance.setData(initData)
+        if (!instance.isValid) {
+            Toast.makeText(this, "lua 非法url", Toast.LENGTH_LONG).show()
+        }
 
     }
 
