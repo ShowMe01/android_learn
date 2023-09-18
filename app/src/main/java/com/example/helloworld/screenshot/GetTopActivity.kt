ae656ef81a9375482b9140cc51f98e229af115e7
ding.feng <ding.feng@hellogroup.com>
2023-09-18 17:52:17 +0800

    提交：本地代码

--- app/src/main/java/com/example/helloworld/screenshot/GetTopActivity.kt
+++ app/src/main/java/com/example/helloworld/screenshot/GetTopActivity.kt
@@ -1,9 +1,15 @@
 package com.example.helloworld.screenshot
 
+import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningTaskInfo
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import com.example.helloworld.R
 import kotlin.concurrent.thread
@@ -15,16 +21,41 @@ class GetTopActivity : AppCompatActivity() {
 
     private val TAG = "GetTopActivity"
 
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_top)
 
+        val switch = findViewById<Switch>(R.id.topSwitch)
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                if (!MyAccessibilityService.hasConnect) {
+                    AlertDialog.Builder(this)
+                        .setMessage("打开我的 \"辅助功能\" 后才能用哦.")
+                        .setPositiveButton("去设置") { dialog, which ->
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_ACCESSIBILITY_SETTINGS
+                            startActivity(intent)
+                        }
+                        .setNegativeButton("取消") { dialog, which -> }
+                        .create()
+                        .show()
+                } else {
+                    loopLogTopActivity()
+                }
+                switch.isChecked = MyAccessibilityService.hasConnect
+            }
+        }
+    }
+
+    private fun loopLogTopActivity() {
         thread {
             val mActivityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
             while (true) {
                 Thread.sleep(1000)
                 try {
-                    val rtis: List<RunningTaskInfo> = mActivityManager.getRunningTasks(1)
+                    val rtis: List<RunningTaskInfo> =
+                        mActivityManager.getRunningTasks(1)
                     val packageName = rtis[0].topActivity?.packageName
                     val topActivityName = rtis[0].topActivity?.className
                     val applicationName =
