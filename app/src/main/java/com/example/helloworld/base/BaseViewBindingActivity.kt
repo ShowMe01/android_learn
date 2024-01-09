92cc72a2273853679d50f39faf4de500738cd159
ding.feng <dif88@foxmail.com>
2024-01-09 23:22:41 +0800

    新增：ipc测试页面

---
+++ app/src/main/java/com/example/helloworld/base/BaseViewBindingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helloworld.base
+
+import android.os.Bundle
+import com.example.helloworld.launchmode.BaseActivity
+
+abstract class BaseViewBindingActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/ipc/TestIPCActivity.kt
@@ -0,0 +1,14 @@
+package com.example.helloworld.ipc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.helloworld.R
+import com.example.helloworld.launchmode.BaseActivity
+
+class TestIPCActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_ipcactivity)
+
+    }
+}
