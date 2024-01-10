a3f8f9c3961692c13db2af9ed3f274776f9bc635
ding.feng <dif88@foxmail.com>
2024-01-10 23:45:20 +0800

    新增：bundle测试页面

--- app/src/main/java/com/example/helloworld/ipc/TestIPCActivity.kt
+++ app/src/main/java/com/example/helloworld/ipc/TestIPCActivity.kt
@@ -1,14 +1,26 @@
 package com.example.helloworld.ipc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.helloworld.R
+import com.example.helloworld.base.BaseViewBindingActivity
+import com.example.helloworld.databinding.ActivityTestIpcactivityBinding
 import com.example.helloworld.launchmode.BaseActivity
 
-class TestIPCActivity : BaseActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test_ipcactivity)
+class TestIPCActivity : BaseViewBindingActivity<ActivityTestIpcactivityBinding>() {
 
+    override fun viewBinding(): ActivityTestIpcactivityBinding {
+        return ActivityTestIpcactivityBinding.inflate(layoutInflater)
+    }
+
+    override fun init() {
+        viewBinding.btnBundle.setOnClickListener {
+            val data = Bundle()
+            data.putString("key1", "stringValue")
+            startActivity(Intent(this, ReceiveBundleActivity::class.java).apply {
+                putExtra("keyExtra", data)
+            })
+        }
     }
 }
