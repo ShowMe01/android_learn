53277a01942c7251a09f37b5de4949b1c0dbb955
ding.feng <dif88@foxmail.com>
2024-05-06 19:31:53 +0800

    新增：测试代码

--- app/src/main/java/com/example/helloworld/media/MediaActivity.kt
+++ app/src/main/java/com/example/helloworld/media/MediaActivity.kt
@@ -1,11 +1,20 @@
 package com.example.helloworld.media
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.media3.common.AudioAttributes
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaController
 import androidx.media3.session.MediaSession
+import androidx.media3.session.SessionCommand
 import androidx.media3.session.SessionToken
 import androidx.media3.ui.PlayerView
 import com.example.helloworld.R
@@ -15,36 +24,65 @@ import com.google.common.util.concurrent.MoreExecutors
 //https://developer.android.com/media/implement/playback-app?hl=zh-cn
 class MediaActivity : BaseActivity() {
 
-
-    lateinit var playerView: PlayerView
+    lateinit var btnPlay: Button
+    lateinit var btnLoopPlay: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_media_activity)
-        playerView = findViewById(R.id.playerView)
+
+        btnPlay = findViewById(R.id.btnPlay)
+        btnPlay.setOnClickListener {
+            initPlayer()
+        }
+        btnLoopPlay = findViewById(R.id.btnLoopPlay)
+        btnLoopPlay.setOnClickListener {
+            val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+            val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+            controllerFuture.addListener(
+                {
+                    // Call controllerFuture.get() to retrieve the MediaController.
+                    // MediaController implements the Player interface, so it can be
+                    // attached to the PlayerView UI component.
+                    val mediaController = controllerFuture.get()
+                },
+                MoreExecutors.directExecutor()
+            )
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
-        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
-        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
-        controllerFuture.addListener(
-            {
-                // Call controllerFuture.get() to retrieve the MediaController.
-                // MediaController implements the Player interface, so it can be
-                // attached to the PlayerView UI component.
-                playerView.player = controllerFuture.get()
-            },
-            MoreExecutors.directExecutor()
-        )
+
     }
 
-    override fun onStop() {
-        super.onStop()
+    private fun initPlayer() {
+        Log.d(PlaybackService.TAG, "initPlayer ")
+        val mediaItem = MediaItem.Builder()
+            .setMediaId("test_sound")
+            .setUri(Uri.parse("asset:///test_sound.mp3"))
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setArtist("DIF")
+                    .setTitle("Muyu4E")
+                    .setArtworkUri(Uri.parse("https://s.momocdn.com/s1/u/ehbehgfg/Mokugyo.jpg"))
+                    .build()
+            )
+            .build()
+        val player = ExoPlayer.Builder(this)
+            .build()
+        player.setMediaItem(mediaItem)
+        player.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                Log.d(PlaybackService.TAG, "onPlaybackStateChanged: ${playbackState}")
+                if (playbackState == ExoPlayer.STATE_ENDED) {
+                    player.release()
+                }
+            }
+        })
+        player.playWhenReady = true
+        player.prepare()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        playerView.player?.release()
-    }
 }
--- app/src/main/java/com/example/helloworld/media/PlaybackService.kt
+++ app/src/main/java/com/example/helloworld/media/PlaybackService.kt
@@ -1,17 +1,29 @@
 package com.example.helloworld.media
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.DefaultMediaNotificationProvider
+import androidx.media3.session.MediaController
 import androidx.media3.session.MediaLibraryService.MediaLibrarySession
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.SessionCommand
+import androidx.media3.session.SessionResult
+import com.google.common.util.concurrent.ListenableFuture
 
 class PlaybackService : MediaSessionService() {
+
+    companion object {
+        const val TAG = "PlaybackService"
+    }
+
     private var mediaSession: MediaSession? = null
-    private var mediaLibSession: MediaLibrarySession? = null
 
     // Create your Player and MediaSession in the onCreate lifecycle event
     @OptIn(UnstableApi::class)
@@ -19,15 +31,36 @@ class PlaybackService : MediaSessionService() {
         setMediaNotificationProvider(DefaultMediaNotificationProvider(this))
         super.onCreate()
         val player = ExoPlayer.Builder(this)
-            .setAudioAttributes(AudioAttributes.DEFAULT, false)
-            .setHandleAudioBecomingNoisy(false)
+//            .setAudioAttributes(AudioAttributes.DEFAULT, false)
+//            .setHandleAudioBecomingNoisy(false)
             .build()
+        player.addListener(object : Player.Listener {
+            override fun onEvents(player: Player, events: Player.Events) {
+                super.onEvents(player, events)
+            }
+        })
         mediaSession = MediaSession.Builder(this, player)
+            .setCallback(object : MediaSession.Callback {
+                override fun onCustomCommand(
+                    session: MediaSession,
+                    controller: MediaSession.ControllerInfo,
+                    customCommand: SessionCommand,
+                    args: Bundle
+                ): ListenableFuture<SessionResult> {
+                    Log.d(
+                        TAG,
+                        "onCustomCommand: controller:${controller}, args:${args}, session:${session}"
+                    )
+                    return super.onCustomCommand(session, controller, customCommand, args)
+                }
+            })
             .build()
+        Log.d(TAG, "onCreate: ${mediaSession} player:${player}")
     }
 
     // Remember to release the player and media session in onDestroy
     override fun onDestroy() {
+        Log.d(TAG, "onDestroy: ")
         mediaSession?.run {
             player.release()
             release()
@@ -36,7 +69,13 @@ class PlaybackService : MediaSessionService() {
         super.onDestroy()
     }
 
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+
+    }
+
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        Log.d(TAG, "onGetSession: ${mediaSession}")
         return mediaSession
     }
 }
